406b403ef4e602d2db81d92ef4aafe54f0d5c45b
Infertux <infertux@infertux.com>
2011-05-10 22:28:28 +0200

    a few more tests

---
+++ tests/CCE-14306-5.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#    This file is part of System Security Checker
+#    Copyright (C) 2011  Infertux <infertux@infertux.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# CCE-ID: CCE-14306-5
+# Orignal-platform: rhel5
+# Modified: 2010-09-26
+# Description: The nosuid option should be enabled or disabled for /dev/shm.
+# Parameters: enabled / disabled
+# Technical-mechanisms: via /etc/fstab
+
+. $(dirname $0)/../lib/functions.sh
+
+FILE /etc/fstab
+
+cat $file | awk '{$2=="/dev/shm" && $4!~"nosuid"}' && \
+    WARNING "Add the option ,nosuid in column 4 for /dev/shm in $file"
+
+exit $ret
+
