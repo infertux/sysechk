109c53e29d272e7f000daa36bad5376ec37d567d
Infertux <infertux@infertux.com>
2011-05-10 15:14:28 +0200

    added chmod script

---
+++ tools/fix_perms.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname $0)/.."
+
+chmod 700 run_tests.sh
+chmod -R 700 lib
+chmod 700 reports
+chmod -R 700 tests
+chmod 700 tools
+chmod 700 tools/build.sh
+chmod 400 tools/cce-*.xml
+[ -f tools/cce.xml ] && chmod 600 tools/cce.xml
+chmod 700 tools/check_env.sh
+chmod 600 tools/extract.xslt
+chmod 700 tools/fix_perms.sh
+[ -d tools/tmp ] && chmod -R 700 tools/tmp
+
+exit 0
+
