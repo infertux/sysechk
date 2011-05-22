04c55f2236dec92c998f5d5d910387dc14a9820f
Infertux <infertux@infertux.com>
2011-05-23 01:26:49 +0200

    three more tests

---
+++ tests/CCE-3660-8.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#    This file is part of System Security Checker
+#    Copyright (C) 2011  Infertux <infertux@infertux.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# CCE-ID: CCE-3660-8
+# Orignal-platform: rhel5
+# Modified: 2010-09-26
+# Description: Remote connections from accounts with empty passwords should be enabled or disabled as appropriate
+# Parameters: enabled / disabled
+# Technical-mechanisms: via /etc/ssh/sshd_config
+
+. $(dirname $0)/../lib/functions.sh
+
+SUDO grep -Eq '^PermitEmptyPasswords\s+no\s*$' /etc/ssh/sshd_config || WARNING \
+    "Add 'PermitEmptyPasswords no' to /etc/ssh/sshd_config"
+
+exit $ret
+
---
+++ tests/CCE-4292-9.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#    This file is part of System Security Checker
+#    Copyright (C) 2011  Infertux <infertux@infertux.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# CCE-ID: CCE-4292-9
+# Orignal-platform: rhel5
+# Modified: 2010-09-26
+# Description: The auditd service should be enabled or disabled as appropriate.
+# Parameters: enabled / disabled
+# Technical-mechanisms: via chkconfig
+
+. $(dirname $0)/../lib/functions.sh
+
+if [ $REDHAT ]; then
+    chkconfig auditd || WARNING \
+    "Enable the auditd service with 'chkconfig auditd on'"
+elif [ $DEBIAN ]; then
+    [ "$(ls /etc/rc?.d/S??auditd)" ] || WARNING \
+    "Enable the auditd service with 'update-rc.d auditd enable'"
+else
+    WARNING "Please ensure that the auditd service is enabled"
+fi
+
+exit $ret
+
