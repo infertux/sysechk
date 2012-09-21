7751ca046437e2c7befde298d8f14c3925076874
Infertux <infertux@infertux.com>
2012-09-21 21:07:27 +0200

    New test

---
+++ tests/CCE-14063-2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#    This file is part of System Security Checker
+#    Copyright (C) 2011-2012  Infertux <infertux@infertux.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# CCE-ID: CCE-14063-2
+# Orignal-platform: rhel5
+# Modified: 2011-10-07
+# Description: The password hashing algorithm should be configured as appropriate.
+# Parameters: hashing algorithm
+# Technical-mechanisms: via PAM
+
+. $(dirname $0)/../lib/functions.sh
+
+GREP '^password\s+sufficient\s+pam_unix.so sha512 shadow nullok try_first_pass use_authtok$' /etc/pam.d/system-auth || \
+    WARNING "Edit the file /etc/pam.d/system-auth to ensure that sha512 is used by the pam unix.so module in the password section"
+
+exit $ret
+
